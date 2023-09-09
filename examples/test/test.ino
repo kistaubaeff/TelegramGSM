62a3c0189dabe30c8b1a26a75d02cf7d85da6254
Наиль К <otladkamid@outlook.com>
2023-09-09 17:21:50 +0300

    upd2

---
+++ examples/test/test.ino
@@ -0,0 +1,34 @@
+#define TINY_GSM_MODEM_SIM800
+#define CHAT_ID ""
+// Теперь подключаем вашу библиотеку
+#include <TelegramGSM.h>
+#include <SoftwareSerial.h>
+SoftwareSerial Serial1(2, 3);
+const char* BOT_TOKEN = "6351313376:AAHdw97IMI4fLAbgf7PtHPWhYJjfPrYQJEM";
+TinyGsm modem(Serial1);
+TinyGsmClientSecure client(modem);
+HttpClient http(client, "api.telegram.org", 443);
+TelegramGSM telegramBot(client, http, BOT_TOKEN);
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  Serial1.begin(115200);
+  delay(3000);
+  if (!modem.init()) {
+    Serial.println("Failed to initialize modem.");
+    while (true);
+  }
+
+  if (!modem.waitForNetwork()) {
+    Serial.println("Failed to connect to network.");
+    while (true);
+  }
+}
+
+void loop() {
+  telegramBot.getUpdates();
+  telegramBot.sendTelegramMessage("Привет от вашего Arduino бота!");
+  delay(10000);
+}
